036f3681496f2938dccf48ff9a2cc1c824011804
salemaziel <mymainemail0501@gmail.com>
2021-01-29 02:14:11 -0800

    ups

--- gatsby-config.js
+++ gatsby-config.js
@@ -130,7 +130,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "259869698",
+        trackingId: "G-G78KG9HCDJ",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
@@ -142,13 +142,13 @@ module.exports = {
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
-        optimizeId: "OPT-W2BVMC3",
+        //optimizeId: "OPT-W2BVMC3",
         // Enables Google Optimize Experiment ID
         experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
         // Set Variation ID. 0 for original 1,2,3....
         variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
         // Defers execution of google analytics script after page load
-        defer: true,
+        defer: false,
         // Any additional optional fields
         sampleRate: 5,
         siteSpeedSampleRate: 10,
