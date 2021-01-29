82f9c3ae668f44df9e480c552af51f80045b15f6
salemaziel <mymainemail0501@gmail.com>
2021-01-29 02:24:15 -0800

    jyjflj

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,9 +35,9 @@ module.exports = {
       {
           resolve: `gatsby-plugin-advanced-sitemap`,
       },
-      {
-        resolve: `gatsby-plugin-canonical-urls`,
-      },
+//      {
+//        resolve: `gatsby-plugin-canonical-urls`,
+//      },
     {
       resolve: "gatsby-plugin-local-search",
       options: {
