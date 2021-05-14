94d002f73a5dc6b86ee65cdafbf9dd4511e95a3a
salemaziel <mymainemail0501@gmail.com>
2021-05-13 17:36:21 -0700

    added matomo analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 //const config = require("./config");
-const config = require('./config/site');
+const config = require("./config/site")
 //const tailwindcss = require('tailwindcss');
 //const autoprefixer = require('autoprefixer');
-
 
 module.exports = {
   siteMetadata: {
@@ -28,28 +27,27 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sharp`,
     },
-      {
-        resolve: `gatsby-plugin-react-helmet`,
-      },
-      {
-        resolve: `gatsby-plugin-sass`,
-//        options: {
-//          postCssPlugins: [
-//            require("tailwindcss"),
-//            require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
-//          ],
-//        },
-      },
-      {
-          resolve: `gatsby-plugin-advanced-sitemap`,
-      },
-      {
-        resolve: `gatsby-plugin-postcss`,
-
-        },
-//      {
-//        resolve: `gatsby-plugin-canonical-urls`,
-//      },
+    {
+      resolve: `gatsby-plugin-react-helmet`,
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      //        options: {
+      //          postCssPlugins: [
+      //            require("tailwindcss"),
+      //            require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+      //          ],
+      //        },
+    },
+    {
+      resolve: `gatsby-plugin-advanced-sitemap`,
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+    },
+    //      {
+    //        resolve: `gatsby-plugin-canonical-urls`,
+    //      },
     {
       resolve: "gatsby-plugin-local-search",
       options: {
@@ -125,9 +123,9 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-//          {
-//            resolve: `gatsby-remark-vscode`,
-//          },
+          //          {
+          //            resolve: `gatsby-remark-vscode`,
+          //          },
           {
             resolve: `gatsby-remark-copy-linked-files`,
           },
@@ -156,9 +154,9 @@ module.exports = {
         // Enables Google Optimize using your container Id
         //optimizeId: "OPT-W2BVMC3",
         // Enables Google Optimize Experiment ID
-//        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        //        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
         // Set Variation ID. 0 for original 1,2,3....
-//        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        //        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
         // Defers execution of google analytics script after page load
         defer: true,
         // Any additional optional fields
@@ -168,38 +166,39 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-robots-txt",
       options: {
-        host: 'https://viadelweb.com',
-        sitemap: 'https://viadelweb.com/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
+        host: "https://viadelweb.com",
+        sitemap: "https://viadelweb.com/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
     },
     {
       resolve: `gatsby-plugin-react-helmet-async`,
     },
     {
-      resolve: 'gatsby-plugin-next-seo',
+      resolve: "gatsby-plugin-next-seo",
       options: {
-        titleTemplate: 'Via Del Web | %s',
-        description: 'Latino Owned Web Design, Web Development, & Digital Marketing Agency based in San Diego, CA specializing in working with small businesses.',
-        language: 'en',
+        titleTemplate: "Via Del Web | %s",
+        description:
+          "Latino Owned Web Design, Web Development, & Digital Marketing Agency based in San Diego, CA specializing in working with small businesses.",
+        language: "en",
         openGraph: {
-          type: 'website',
-          locale: 'en_US',
-          url: 'https://viadelweb.com',
-          site_name: 'Via Del Web',
+          type: "website",
+          locale: "en_US",
+          url: "https://viadelweb.com",
+          site_name: "Via Del Web",
           images: [
             {
-              url: 'https://viadelweb.com/logo/logo.png',
-              alt: 'Via Del Web Logo',
-            }
-          ]
+              url: "https://viadelweb.com/logo/logo.png",
+              alt: "Via Del Web Logo",
+            },
+          ],
         },
         twitter: {
-          handle: '@viadelweb',
-          site: '@site',
-          cardType: 'summary_large_image',
+          handle: "@viadelweb",
+          site: "@site",
+          cardType: "summary_large_image",
         },
       },
     },
@@ -208,22 +207,39 @@ module.exports = {
       options: {
         mailgoConfig: {
           dark: true,
-        }
-      }
+        },
+      },
     },
-//    {
-//      resolve: `gatsby-plugin-mautic`,
-//      options: {
-        // The URL where mautic is installed
-//        hostUrl: "https://marketing.viadelweb.agency",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-//        head: false,
-        // Avoids sending pageview hits from custom paths
-//        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-//        pageTransitionDelay: 0,
-//      },
-//    },
+    //    {
+    //      resolve: `gatsby-plugin-mautic`,
+    //      options: {
+    // The URL where mautic is installed
+    //        hostUrl: "https://marketing.viadelweb.agency",
+    // Defines where to place the tracking script - `true` in the head and `false` in the body
+    //        head: false,
+    // Avoids sending pageview hits from custom paths
+    //        exclude: ["/preview/**", "/do-not-track/me/too/"],
+    // Delays sending pageview hits on route update (in milliseconds)
+    //        pageTransitionDelay: 0,
+    //      },
+    //    },
+    {
+      resolve: "gatsby-plugin-matomo",
+      options: {
+        siteId: "2",
+        matomoUrl: "https://analytics.viadelweb.cloud",
+        siteUrl: "https://viadelweb.com",
+        // All the optional settings
+//        matomoPhpScript: "piwik.php",
+//        matomoJsScript: "piwik.js",
+        exclude: ["/offline-plugin-app-shell-fallback/"],
+        requireConsent: false,
+        disableCookies: true,
+        //cookieDomain: "*.example.org",
+//        localScript: "/piwik.js",
+        dev: false,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -236,9 +252,9 @@ module.exports = {
         icon: `src/images/logo-only.png`, // This path is relative to the root of the site.
       },
     },
-//    {
-//      resolve: `gatsby-plugin-offline`,
-//    },
+    //    {
+    //      resolve: `gatsby-plugin-offline`,
+    //    },
     {
       resolve: `gatsby-plugin-sitemap`,
     },
